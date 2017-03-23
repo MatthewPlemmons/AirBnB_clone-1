56861f01f8a760563a71e19917f62ddb9aa29b39
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-03-23 06:47:05 +0000

    update review model

--- models/review.py
+++ models/review.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import *
 
 
-class Review(BaseModel):
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Model for managing review data"""
+    __tablename__ = "reviews"
+    place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
+    user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+    text = Column(String(1024), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
