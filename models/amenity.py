3ab0bc27593a7a2295b8ac28eb31b8f323c67ef7
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-03-23 06:35:32 +0000

    update amenity model

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Model for storing amenity data"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
