809eb29d484b781f0a24dd3a0baed567c692d4bb
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-03-23 06:51:57 +0000

    update state model

--- models/state.py
+++ models/state.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    name = ""
+class State(BaseModel, Base):
+    """Model for managing state data"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
     def __init__(self, *args, **kwargs):
         super(State, self).__init__(*args, **kwargs)
