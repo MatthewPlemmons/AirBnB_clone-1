2ade9e4ab56801c1fcaf2c5efbeb1f553f2c2243
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-03-23 06:26:44 +0000

    update base_model for sqlalchemy"

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-import datetime
 import uuid
 import models
+from datetime import datetime
+from sqlalchemy.ext.declatative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """The base class for all storage objects in this project"""
+    id = (String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime(), nullable=False, default=datetime.now())
+    updated_at = Column(DateTime(), nullable=False, default=datetime.now())
+
+
     def __init__(self, *args, **kwargs):
         """initialize class object"""
         if len(args) > 0:
@@ -15,13 +24,15 @@ class BaseModel:
             self.created_at = datetime.datetime.now()
             self.id = str(uuid.uuid4())
         for k in kwargs:
-            print("kwargs: {}: {}".format(k, kwargs[k]))
+            self.k = kwargs[k]
+
 
     def save(self):
         """method to update self"""
         self.updated_at = datetime.datetime.now()
         models.storage.new(self)
         models.storage.save()
+
 
     def __str__(self):
         """edit string representation"""
@@ -31,8 +42,15 @@ class BaseModel:
     def to_json(self):
         """convert to json"""
         dupe = self.__dict__.copy()
+        try: 
+            dupe.pop("_sa_instnace_state", None)
+        except:
+            pass
         dupe["created_at"] = str(dupe["created_at"])
         if ("updated_at" in dupe):
             dupe["updated_at"] = str(dupe["updated_at"])
         dupe["__class__"] = type(self).__name__
         return dupe
+
+    def delete(self):
+        models.storage.delete(self.id)
