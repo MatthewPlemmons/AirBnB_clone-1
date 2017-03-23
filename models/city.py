444ef3294f5a4a630f414ad9cdc45d816a39bcba
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-03-23 06:42:14 +0000

    update city model

--- models/city.py
+++ models/city.py
@@ -2,9 +2,11 @@
 from models import *
 
 
-class City(BaseModel):
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """Model for storing city data"""
+    __tablename__ = "cities"
+    state_id = Column(String(60), nullable=False, ForeignKey("states.id")
+    name = Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
