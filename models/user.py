1a3e4314a94a9bbbb7da01048a7ab1582de8a188
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-03-23 06:56:14 +0000

    update user model

--- models/user.py
+++ models/user.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    """Model for managing user data"""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="delete", backref="user")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
