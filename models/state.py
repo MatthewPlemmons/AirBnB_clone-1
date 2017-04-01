11823f6143c7f2522d74a2daf83d7846e9913664
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-31 23:00:48 -0500

    update state

--- models/state.py
+++ models/state.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 from os import getenv
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from models import *
 from models.base_model import BaseModel, Base
 
 
 class State(BaseModel, Base):
     """Model for managing state data"""
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
@@ -16,10 +15,11 @@ class State(BaseModel, Base):
     else:
         name = ""
 
+    def __init__(self, *args, **kwargs):
+        super(State, self).__init__(*args, **kwargs)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             city_list = storage.all("City").values()
             return [city for city in city_list if self.id == city.state_id]
-
-    def __init__(self, *args, **kwargs):
-        super(State, self).__init__(*args, **kwargs)
